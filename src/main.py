56a3d6a8c53a6e5bd828d42e76db12fc7768eccd
Anton Briganti <anton.briganti@gmail.com>
2020-08-19 18:15:39 +1000

    added tinydb support

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from csv_helper import CSVHandler
 import sys
+from tinydb import TinyDB, Query
+from csv_helper import CSVHandler
 
 def format_release_data(filename):
     data = CSVHandler().convert_csv_to_dict(filename)
@@ -7,10 +8,29 @@ def format_release_data(filename):
         entry['Genre'] = [genre.strip() for genre in entry['Genre'].split("/")]
     return data
 
+def update_db(data, table):
+    for release in data:
+        if not release_already_in_db(release, table):
+            table.insert(release)
+
+def release_already_in_db(release, table):
+    Release = Query()
+    return len(table.search(Release.Release == release["Release"])) > 0
+
+
 if __name__ == "__main__":
-    if (len(sys.argv) > 2):
+    if (len(sys.argv) >= 2):
         source_file = sys.argv[1]
         print("loading", source_file)
         release_data = format_release_data(source_file)
+
+        # intialise db
+        print("intialising db") 
+        db = TinyDB('music.json')
+        release_db_table = db.table('releases')
+
+        # update db
+        print("updating db")
+        update_db(release_data, release_db_table)
     
     
